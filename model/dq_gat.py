f3521362bf636450de6e7290d5c3cfa84118d303
Jiaqi1song <jiaqison@andrew.cmu.edu>
2025-03-11 22:46:29 -0400

    add custum network

--- model/dq_gat.py
+++ model/dq_gat.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet18
 from model.agent_state import VocabularyStateType
+from typing import Callable, Dict, List, Optional, Tuple, Type, Union
+from gymnasium import spaces
+from stable_baselines3.common.policies import ActorCriticPolicy
 
 class ResNetEncoder(nn.Module):
     """CNN-based feature extractor using ResNet-18"""
@@ -99,3 +102,77 @@ class DQGAT(nn.Module):
         ego_node_feature = scene_graph_embedding[:, 0, :] 
 
         return ego_node_feature
+
+
+class CustomNetwork(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the features extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+        last_layer_dim_pi: int = 256,
+        last_layer_dim_vf: int = 256,
+    ):
+        super().__init__()
+
+        # IMPORTANT:
+        # Save output dimensions, used to create the distributions
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+        
+        # DQ-GAT
+        self.DQGAT = DQGAT()
+
+        # Policy network
+        self.policy_net = nn.Sequential(
+            nn.Linear(feature_dim, last_layer_dim_pi), nn.ReLU()
+        )
+        # Value network
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim, last_layer_dim_vf), nn.ReLU()
+        )
+
+    def forward(self, obs: Dict) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        features = self.DQGAT.forward(obs["bev"], obs["agent"])
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: torch.Tensor) -> torch.Tensor:
+        return self.policy_net(features)
+
+    def forward_critic(self, features: torch.Tensor) -> torch.Tensor:
+        return self.value_net(features)
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        lr_schedule: Callable[[float], float],
+        *args,
+        **kwargs,
+    ):
+        # Disable orthogonal initialization
+        kwargs["ortho_init"] = False
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomNetwork(feature_dim=256)
